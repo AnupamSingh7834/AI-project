831ff6e684d480ec4b9b319f26b5a80877ad919e
AnupamSingh7834 <130218755+AnupamSingh7834@users.noreply.github.com>
2023-04-20 00:03:12 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -6,8 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'O
-    nlineShopping.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShopping.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
